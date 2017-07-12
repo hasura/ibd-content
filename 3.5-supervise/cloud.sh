96674cafa2f59543963fc416e75821dfb6c24e0f
Tanmai Gopal <tanmaig@34cross.in>
2017-07-12 23:49:25 +0530

    Changes wget to curl because minikube wget doesn't support https

--- 3.5-supervise/cloud.sh
+++ 3.5-supervise/cloud.sh
@@ -1,3 +1,3 @@
-wget https://raw.githubusercontent.com/hasura/ibd-content/master/3.5-supervise/ibd-coreos.service && \
+curl https://raw.githubusercontent.com/hasura/ibd-content/master/3.5-supervise/ibd-coreos.service > ibd-coreos.service && \
     sudo mv ibd-coreos.service /etc/systemd/system/ && \
     sudo systemctl enable ibd-coreos.service
