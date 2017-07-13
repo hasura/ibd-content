59d5e9d7141e29e0009c7100e8b3ef5c640dc101
Tanmai Gopal <tanmaig@34cross.in>
2017-07-13 21:52:06 +0530

    Adds commands and scripts for 4.1 docker exercise

---
+++ 4.1-docker/all.sh
@@ -0,0 +1,7 @@
+#node
+./node-server.js #fails
+docker run -ti hasura/ibd-node-server:0.1 #runs
+
+#python
+./python-server.py #fails
+docker run -ti hasura/ibd-python-server:0.1 #runs
---
+++ 4.1-docker/set-minihasura-docker-env.sh
@@ -0,0 +1,5 @@
+export DOCKER_TLS_VERIFY=1
+export DOCKER_CERT_PATH=/Users/tanmaigopal/.minihasura/certs
+export DOCKER_HOST=tcp://192.168.99.100:2376
+export DOCKER_MACHINE_NAME=hasura
+export DOCKER_API_VERSION=1.23
